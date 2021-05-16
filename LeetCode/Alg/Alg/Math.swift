4d84446e914345cd90684b91493ef8a5c91d0848
chendong <derek.chen@vova.com.hk>
2021-05-16 16:29:01 +0800

    update

--- LeetCode/Alg/Alg/Math.swift
+++ LeetCode/Alg/Alg/Math.swift
@@ -74,113 +74,197 @@ func Math() {
     
     //    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn4n7c/
     //    罗马数字转整数
+    //    class Solution {
+    //        func romanToInt(_ s: String) -> Int {
+    //            let obj = [
+    //                "I": 1,
+    //                "V": 5,
+    //                "X": 10,
+    //                "L": 50,
+    //                "C": 100,
+    //                "D": 500,
+    //                "M": 1000,
+    //
+    //                "IV": 4,
+    //                "IX": 9,
+    //                "XL": 40,
+    //                "XC": 90,
+    //                "CD": 400,
+    //                "CM": 900,
+    //            ];
+    //
+    //            var res = 0
+    //            var index = s.startIndex
+    //            while index < s.endIndex {
+    //                let next2Index = s.index(index, offsetBy: 2, limitedBy: s.endIndex)
+    //                if let nx = next2Index, let num = obj[String(s[index..<nx])]  {
+    //                    res += num
+    //                    index = nx
+    //                } else {
+    //                    let next1Index = s.index(after: index)
+    //                    let num = obj[String(s[index..<next1Index])]!
+    //                    res += num
+    //                    index = next1Index
+    //                }
+    //            }
+    //            return res
+    //        }
+    //    }
+    //    Solution().romanToInt("MCMXCIV")
+    
+    //    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn1m0i/
+    //    位1的个数
+    //    class Solution {
+    //        func hammingWeight(_ n: Int) -> Int {
+    //            var res = 0
+    //            var num = n
+    //            while num > 0 {
+    ////                res += num & 1
+    ////                num = num >> 1
+    //
+    ////                num &= num - 1
+    //                res += 1
+    //            }
+    //            return res
+    //        }
+    //    }
+    //    Solution().hammingWeight(0b00000000000000000000000000001011)
+    
+    //    class Solution {
+    //        func hammingDistance(_ x: Int, _ y: Int) -> Int {
+    //
+    //        }
+    //    }
+    
+    //    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnyode/
+    //    汉明距离
+    //    class Solution {
+    //        func hammingDistance(_ x: Int, _ y: Int) -> Int {
+    //            var dif = x ^ y
+    //            var res = 0
+    //            while dif > 0 {
+    //                res += dif & 1
+    //                dif = dif >> 1
+    //            }
+    //            return res
+    //        }
+    //    }
+    //    print(Solution().hammingDistance(1, 4))
+    
+    
+    //    颠倒二进制位
+    //    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnc5vg/
+    //    class Solution {
+    //        func reverseBits(_ n: Int) -> Int {
+    //            var res = 0
+    //            var num = n
+    //            for _ in 0..<32 {
+    //                res <<= 1
+    //                res |= num & 1
+    //                num >>= 1
+    //            }
+    //            return res
+    //        }
+    //    }
+    //    Solution().reverseBits(2)
+    
+    //    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xncfnv/
+    //    杨辉三角
+    
+    //    class Solution {
+    //        func generate(_ numRows: Int) -> [[Int]] {
+    //            if numRows == 1 {
+    //                return [[1]]
+    //            }
+    //            var nums = generate(numRows - 1)
+    //            let preArr = nums.last!
+    //            var newArr = [1]
+    //            for i in 0..<preArr.count - 1 {
+    //                newArr.append(preArr[i] + preArr[i + 1])
+    //            }
+    //            newArr.append(1)
+    //            nums.append(newArr)
+    //            return nums
+    //        }
+    //    }
+    //    print(Solution().generate(4))
+    
+    //    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnbcaj/
+    //    有效的括号
+    
 //    class Solution {
-//        func romanToInt(_ s: String) -> Int {
-//            let obj = [
-//                "I": 1,
-//                "V": 5,
-//                "X": 10,
-//                "L": 50,
-//                "C": 100,
-//                "D": 500,
-//                "M": 1000,
+//        func isValid(_ s: String) -> Bool {
+            //            let pairs = ["[":"]","{":"}","(":")"]
+            //            var stack = [String]()
+            //            for slice in s {
+            //                let sliceStr = String(slice)
+            //                if let pair = pairs[sliceStr] {
+            //                    stack.append(pair)
+            //                } else {
+            //                    if let pair =  stack.popLast() {
+            //                        if pair != sliceStr {
+            //                            return false
+            //                        }
+            //                    } else {
+            //                        return false
+            //                    }
+            //                }
+            //            }
+            //            return stack.isEmpty
+            
+//            if s.count % 2 > 0 {
+//                return false
+//            }
 //
-//                "IV": 4,
-//                "IX": 9,
-//                "XL": 40,
-//                "XC": 90,
-//                "CD": 400,
-//                "CM": 900,
-//            ];
+//            let map: [Character: Character] = [
+//                Character("}"): Character("{"),
+//                Character(")"): Character("("),
+//                Character("]"): Character("[")
+//            ]
 //
-//            var res = 0
-//            var index = s.startIndex
-//            while index < s.endIndex {
-//                let next2Index = s.index(index, offsetBy: 2, limitedBy: s.endIndex)
-//                if let nx = next2Index, let num = obj[String(s[index..<nx])]  {
-//                    res += num
-//                    index = nx
+//
+//            var stack: [Character] = []
+//
+//            for c in Array(s) {
+//
+//                if let val = map[c] {
+//                    if val != stack.popLast() { return false }
 //                } else {
-//                    let next1Index = s.index(after: index)
-//                    let num = obj[String(s[index..<next1Index])]!
-//                    res += num
-//                    index = next1Index
+//                    stack.append(c)
 //                }
-//            }
-//            return res
-//        }
-//    }
-//    Solution().romanToInt("MCMXCIV")
-    
-//    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn1m0i/
-//    位1的个数
-//    class Solution {
-//        func hammingWeight(_ n: Int) -> Int {
-//            var res = 0
-//            var num = n
-//            while num > 0 {
-////                res += num & 1
-////                num = num >> 1
 //
-////                num &= num - 1
-//                res += 1
 //            }
-//            return res
-//        }
-//    }
-//    Solution().hammingWeight(0b00000000000000000000000000001011)
-    
-//    class Solution {
-//        func hammingDistance(_ x: Int, _ y: Int) -> Int {
 //
+//            return stack.isEmpty
 //        }
 //    }
+//    print(Solution().isValid("()[]{}"))
     
-//    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnyode/
-//    汉明距离
-//    class Solution {
-//        func hammingDistance(_ x: Int, _ y: Int) -> Int {
-//            var dif = x ^ y
-//            var res = 0
-//            while dif > 0 {
-//                res += dif & 1
-//                dif = dif >> 1
-//            }
-//            return res
+//    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnj4mt/
+//    缺失数字
+//    public int missingNumber(int[] nums) {
+//        if(nums.length <= 0){
+//            return 0;
 //        }
-//    }
-//    print(Solution().hammingDistance(1, 4))
-    
-    
-//    颠倒二进制位
-//    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnc5vg/
-    
-//    public int reverseBits(int n) {
-//        int res = 0;
-//        for (int i = 0; i < 32; i++) {
-//            //res先往左移一位，把最后一个位置空出来，
-//            //用来存放n的最后一位数字
-//            res <<= 1;
-//            //res加上n的最后一位数字
-//            res |= n & 1;
-//            //n往右移一位，把最后一位数字去掉
-//            n >>= 1;
+//        int reduce = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            reduce = reduce ^ nums[i] ^ i;
 //        }
-//        return res;
+//        return reduce ^ nums.length;
 //    }
-
+    
     class Solution {
-        func reverseBits(_ n: Int) -> Int {
-            var res = 0
-            var num = n
-            for _ in 0..<32 {
-                res <<= 1
-                res |= num & 1
-                num >>= 1
+        func missingNumber(_ nums: [Int]) -> Int {
+            if nums.count < 1 {
+                return 0
             }
+            var reduce = 0
+            for i in 0..<nums.count {
+                reduce = reduce ^ i ^ nums[i]
+            }
+            let res = reduce ^ nums.count
             return res
         }
     }
-    Solution().reverseBits(2)
-
+    Solution().missingNumber([0,1,3])
 }
