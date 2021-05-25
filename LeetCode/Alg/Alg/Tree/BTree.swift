0d0cd8cd29a4113409942e497bbba0da84c2d4f7
chendong <derek.chen@vova.com.hk>
2021-05-25 23:07:37 +0800

    update

--- LeetCode/Alg/Alg/Tree/BTree.swift
+++ LeetCode/Alg/Alg/Tree/BTree.swift
@@ -118,8 +118,8 @@ func BTree() {
 //        print(node.val,terminator: " ")
 //    }
 //
-    class Solution {
-
+//    class Solution {
+        
 //        func postorderTraversal(_ root: TreeNode<Any>?) -> [Int] {
 //            var res = [Int]()
 //            n_ergodic(node: root, res: &res)
@@ -138,9 +138,115 @@ func BTree() {
 //            }
 //        }
         
+        /*
+         1
+         2     3
+         1 2   1  2
         
-        func postorderTraversal(_ root: TreeNode<Any>?) -> [Int] {
-            var res = [Int]()
+         public List<Integer> postorderTraversal(TreeNode root) {
+             List<Integer> res = new ArrayList<>();
+             if(root == null)
+                 return res;
+             Deque<TreeNode> stack = new LinkedList<>();
+             stack.push(root);
+             while(!stack.isEmpty()){
+                 root = stack.pop();
+                 if(root != null){
+                     stack.push(root);
+                     stack.push(null);
+                     if(root.right != null)
+                         stack.push(root.right);
+                     if(root.left != null)
+                         stack.push(root.left);
+                 }
+                 else
+                     res.add(stack.pop().val);
+             }
+             return res;
+         }
+         */
+        
+//        func postorderTraversal(_ root: TreeNode<Any>?) -> [Int] {
+//            guard let n = root else {
+//                return [Int]()
+//            }
+//            var res = [Int]()
+//            var stack = [n]
+//            while !stack.isEmpty {
+//                let node = stack.popLast()!
+//                res.append(node.val as! Int)
+//                if let left = node.left {
+//                    stack.append(left)
+//                }
+//                if let right = node.right {
+//                    stack.append(right)
+//                }
+//            }
+//            res = res.reversed()
+//            return res
+//        }
+//    }
+    
+//    层序遍历
+//    https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xej9yc/
+//    二叉树的层序遍历
+//    https://leetcode-cn.com/leetbook/read/data-structure-binary-tree/xefh1i/
+    
+    /*
+     public List<List<Integer>> levelOrder(TreeNode root) {
+         List<List<Integer>> result = new ArrayList<>();
+         if (root == null) {
+             return result;
+         }
+         Queue<TreeNode> queue = new LinkedList<>();
+         // 先将根节点放入队列
+         queue.add(root);
+         while (queue.size() > 0) {
+             int size = queue.size();
+             List<Integer> temp = new ArrayList<>();
+             // 遍历队列，把当前层的元素从队列取出来，将下一层放入队列
+             for (int i = 0; i < size; i++) {
+                 // 取出队列元素，放入集合
+                 TreeNode current = queue.poll();
+                 temp.add(current.val);
+                 if (current.left != null) {
+                     // 将当前节点的左儿子放入队列
+                     queue.add(current.left);
+                 }
+                 if (current.right != null) {
+                     // 将当前节点的右儿子放入队列
+                     queue.add(current.right);
+                 }
+             }
+             result.add(temp);
+         }
+         return result;
+     }
+     */
+    class Solution {
+        func levelOrder(_ root: TreeNode<Any>?) -> [[Any]] {
+            var res = [[Any]]()
+            guard let n = root else {
+                return res
+            }
+            
+            var queue = [n]
+            while !queue.isEmpty {
+                var nextLevel = [TreeNode<Any>]()
+                var vals = [Any]()
+                for i in 0..<queue.count {
+                    let node = queue[i]
+                    vals.append(node.val)
+                    if let left = node.left {
+                        nextLevel.append(left)
+                    }
+                    if let right = node.right {
+                        nextLevel.append(right)
+                    }
+                }
+                queue = nextLevel
+                res.append(vals)
+            }
             
             return res
         }
