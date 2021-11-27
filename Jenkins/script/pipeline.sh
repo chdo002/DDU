1ed3c3b2dd290e317b4d77e5d75029d7d57f5ecf
chdo002 <1107661983@qq.com>
2021-11-27 16:35:58 +0800

    update

--- Jenkins/script/pipeline.sh
+++ Jenkins/script/pipeline.sh
@@ -2,15 +2,21 @@
 
 echo "联系来了-->"
 
+
+BRANCH=$1
+GIT_REPOSITORY=$2
+VERSION=$3
+
 echo "参数--"
 echo "$0"
-echo "$1"
-echo "$2"
-echo "$3"
-echo "$4"
+echo "$BRANCH"
+echo "$GIT_REPOSITORY"
+echo "$VERSION"
 
 echo "路径--"
 pwd
 ls
+
+git clone GIT_REPOSITORY
 
 echo "联系来了<--"
