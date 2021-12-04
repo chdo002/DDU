53a908d4194f47c0b2d67e9eecd4703211f276c0
chdo002 <1107661983@qq.com>
2021-12-04 12:04:24 +0800

    update

--- Shell/demo.sh
+++ Shell/demo.sh
@@ -1,4 +1,29 @@
 #!/bin/sh
+
+. Shell/demo2.sh
+
+thisIsFunction "??"
+
+exit
+
+echo<'Shell/1.txt' >> Shell/1.txt
+exit;
+
+res=$[1+2]
+echo $res
+
+exit;
+
+num1=100
+num2=100
+if test $num1 -eq $num2
+then
+    echo '两个数相等！'
+else
+    echo '两个数不相等！'
+fi
+exit;
+
 a=1
 b=1
 c=12
@@ -20,3 +45,6 @@ echo "第一个元素为: ${my_array[0]}"
 echo "第二个元素为: ${my_array[1]}"
 echo "第三个元素为: ${my_array[2]}"
 echo "第四个元素为: ${my_array[3]}"
+
+val=`expr 2 + 2`
+echo "两数之和为 : $val"
