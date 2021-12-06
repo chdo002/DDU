21b81d8da1512e9ed0c467daa2d8bd63a523e093
chdo002 <1107661983@qq.com>
2021-12-06 21:14:03 +0800

    update

---
+++ flutter_demo/pure_flutter/lib/PageA.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PageA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("123")), body: Wraper());
+  }
+}
+
+class Wraper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.of(context).pop("fffm");
+        },
+        child: Container(
+            color: Colors.blueGrey,
+            child: Center(
+              child: Text("Second -> Page",
+                  style: TextStyle(
+                      color: Colors.white, backgroundColor: Colors.red),
+                  textDirection: TextDirection.ltr),
+            )));
+  }
+}
--- flutter_demo/pure_flutter/lib/main.dart
+++ flutter_demo/pure_flutter/lib/main.dart
@@ -1,11 +1,60 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/PageA.dart';
 
 void main() {
-  runApp(
-    const Center(
-      child: Text(
-        'Hello, world!'
-      ),
-    ),
-  );
+  runApp(MainVC());
+}
+
+class MainVC extends StatelessWidget {
+  const MainVC({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        routes: {
+          "pageA": (context) {
+            return PageA();
+          }
+        },
+        home: Scaffold(
+            appBar: AppBar(title: const Text("rootPage")), body: Wraper()));
+  }
+}
+
+class Wraper extends StatelessWidget {
+  Widget redBox = DecoratedBox(decoration: BoxDecoration(color: Colors.red));
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () async {
+          Navigator.pushNamed(context, "pageA");
+          Future.microtask(() => {
+                // print("132123");
+              });
+        },
+        child: Container(
+            color: Colors.teal,
+            child: Center(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.network(
+                  "https://avatars2.githubusercontent.com/u/20411648?s=460&v=4",
+                  width: 100.0,
+                ),
+                ConstrainedBox(
+                    constraints: BoxConstraints(minWidth: 60, minHeight: 100.0),
+                    child: UnconstrainedBox(
+                        child: ConstrainedBox(
+                            constraints:
+                                BoxConstraints(minWidth: 100, minHeight: 20),
+                            child: Container(height: 5, child: redBox)))),
+                const Text("First -> Page1",
+                    style: TextStyle(
+                        color: Colors.white, backgroundColor: Colors.lightBlue),
+                    textDirection: TextDirection.ltr)
+              ],
+            ))));
+  }
 }
