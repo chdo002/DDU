0334b6b8612db7fcf997481c332c1b51d8106626
chdo002 <1107661983@qq.com>
2021-12-14 19:13:08 +0800

    update

--- flutter_demo/pure_flutter/ios/Flutter/flutter_export_environment.sh
+++ flutter_demo/pure_flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/chendong/flutter"
+export "FLUTTER_ROOT=/Users/chendong/development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/chendong/Desktop/DDU/flutter_demo/pure_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=/Users/chendong/Desktop/DDU/flutter_demo/pure_flutter/lib/main.dart"
