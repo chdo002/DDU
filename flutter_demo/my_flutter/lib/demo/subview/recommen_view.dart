23c2b0cdb10faf62b2a7c75ed981ba2752902d10
chdo002 <1107661983@qq.com>
2021-12-30 11:07:32 +0800

    修改

--- flutter_demo/my_flutter/lib/demo/subview/recommen_view.dart
+++ flutter_demo/my_flutter/lib/demo/subview/recommen_view.dart
@@ -21,24 +21,39 @@ class HSQSelectRecommendView extends StatelessWidget {
           height: 200,
           child: Column(children: [
             Row(children: [
-              Text("精选推荐", style: TextStyle(fontWeight: FontWeight.bold)),
+              const Text("精选推荐", style: TextStyle(fontWeight: FontWeight.bold)),
               Container(width: 5),
-              Text('店铺热门爆款',
+              const Text('店铺热门爆款',
                   style: TextStyle(
                       fontWeight: FontWeight.w400, color: Colors.grey)),
               Expanded(
                   child: Row(
                       mainAxisAlignment: MainAxisAlignment.end,
-                      children: [Text('查看全部')]))
+                      children: const [Text('查看全部')]))
             ]),
             Expanded(
                 child: ListView.builder(
                     scrollDirection: Axis.horizontal,
-                    itemCount: 2,
-                    itemBuilder: (context, constrains) {
-                      return Text("123");
+                    itemCount: model.items.length,
+                    itemBuilder: (context, index) {
+                      return HSQRecommendItemView(model.items[index]);
                     }))
           ]));
     });
+  }
+}
+
+class HSQRecommendItemView extends StatelessWidget {
+  const HSQRecommendItemView(this.item, {Key? key}) : super(key: key);
+  final RecommendItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 100,
+        child: Column(children: [
+          AspectRatio(aspectRatio: 1, child: Image(image: NetworkImage(item.thumbnail ?? ''))),
+          Expanded(child: Column(children: [Text(item.name ?? '')]))
+        ]));
   }
 }
