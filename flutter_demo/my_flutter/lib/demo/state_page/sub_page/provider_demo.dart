48fce28f32073579f881073f846726f6bac7d265
chdo002 <1107661983@qq.com>
2022-01-16 20:39:59 +0800

    update

--- flutter_demo/my_flutter/lib/demo/state_page/sub_page/provider_demo.dart
+++ flutter_demo/my_flutter/lib/demo/state_page/sub_page/provider_demo.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ProviderDemo extends StatelessWidget {
@@ -16,10 +19,14 @@ class ProviderDemo extends StatelessWidget {
                 },
               ),
               CupertinoButton(
-                  child: const Text('点'),
+                  child: const Text('加'),
                   onPressed: () {
                     Provider.of<CartViewModel>(c, listen: false).addCount();
-                  })
+                  }),
+              const Divider(color: Colors.black),
+              _ValueDemoPage(),
+              const Divider(color: Colors.black),
+              _ValueListenenableDemoPage()
             ]);
           }),
     ));
@@ -31,5 +38,41 @@ class CartViewModel extends ChangeNotifier {
   addCount() {
     count += 1;
     notifyListeners();
+  }
+}
+
+class _ValueDemoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+        value: CartViewModel(),
+        builder: (context, wid) {
+          return Column(children: [
+            Text("Value demo:${Provider.of<CartViewModel>(context).count}"),
+            CupertinoButton(
+                color: Colors.teal,
+                child: const Text('点击'),
+                onPressed: () {
+                  Provider.of<CartViewModel>(context, listen: false).addCount();
+                })
+          ]);
+        });
+  }
+}
+
+class _ValueListenenableDemoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: ValueNotifier(CartViewModel()),
+        builder: (context, cartVM, widget) {
+          return Column(children: [
+            Text('Value'),
+            CupertinoButton(
+              child: Text("123"),
+              onPressed: () {},
+            )
+          ]);
+        });
   }
 }
