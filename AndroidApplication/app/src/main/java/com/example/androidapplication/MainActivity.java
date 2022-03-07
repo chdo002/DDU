a2c940f1fc38c0f4969ac6d94187eda41f2b7c03
chdo002 <1107661983@qq.com>
2022-03-07 19:42:19 +0800

    recycleview

--- AndroidApplication/app/src/main/java/com/example/androidapplication/MainActivity.java
+++ AndroidApplication/app/src/main/java/com/example/androidapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -39,8 +40,10 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//                Intent intent = new Intent(MainActivity.this, com.example.androidapplication.RecycleViewActivity.class);
+//                startActivity(intent);
             }
         });
     }
