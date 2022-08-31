c98adfc11eec7dbaa66cef2299d65cde2b566eed
chdo002 <11076619833@qq.com>
2022-08-31 09:30:39 +0800

    update

--- dart_demo/dart_lession/async_await.dart
+++ dart_demo/dart_lession/async_await.dart
@@ -1,15 +1,61 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:isolate';
+import 'dart:math';
+
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
+log(String msg) => print('${DateTime.now()}: $msg');
+
 void main(List<String> args) async {
-  print("A");
-  testRequest().then((value) {
-    print(value.statusCode);
+  isolateTest();
+}
+
+String global = "原本值";
+
+// https://www.jianshu.com/p/5ba4ffc1d4ac
+isolateTest() async {
+  // log("A");
+  // testRequest().then((value) {
+  //   log(value.statusCode);
+  // });
+  // log("B");
+  // log('A');
+  // asyncF1();
+  global = '初始值';
+  final action = (SendPort port) {
+    log(global);
+    global = "新值2";
+    port.send(global);
+  };
+  ReceivePort port = ReceivePort();
+  var iso = await Isolate.spawn(action, port.sendPort);
+  port.listen((message) {
+    log(message);
+    port.close();
+    iso.kill();
   });
-  print("B");
+  // log('B');
+  // sleep(Duration(seconds: 2));
+  // log('C');
+  // asyncF2();
+  // log('D');
+}
+
+Future<String> asyncF1() async {
+  sleep(Duration(seconds: 1));
+  global = '新值';
+  log('global:$global  asyncF1,,??,${Isolate.current.debugName}');
+  return global;
+}
+
+Future<void> asyncF2() async {
+  log('global:$global  asyncF2,,??,${Isolate.current.debugName}');
 }
 
 Future<Response> testRequest() {
   final res = http.get(Uri.parse('http://www.baidu.com'));
+  log(',,??,${Isolate.current.debugName}');
   return res;
 }
